f63d2382e0d74cba5c947611ea1eb7cbaeb25e14
Diego Bevilacqua <dbevilacqua@asap-consulting.com.ar>
2021-09-02 11:20:37 -0300

    .

--- src/__tests__/webcamCapture.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import React from 'react'
-import WebcamCapture from '../Components/WebcamCapture'
-
-test("rendering WebcamCapture", () => {
-    render(<WebcamCapture />)
-    expect(screen.getByText(/Capture/i)).toBeInTheDocument()
-})
